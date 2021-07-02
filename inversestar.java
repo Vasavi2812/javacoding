b312fd133c14681ac864cf631c8b233288652192
Vasavi2812 <86615056+Vasavi2812@users.noreply.github.com>
2021-07-02 06:56:36 +0530

    Create inversestar.java

---
+++ inversestar.java
@@ -0,0 +1,16 @@
+class inversestar
+{
+public static void main(String args[])
+{
+int i,j,N=5;
+for(i=1;i<=N;i++)
+{
+for(j=0;j<=N-i;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
+
