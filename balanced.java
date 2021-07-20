827e8afcb28f2f38e5ee0cb9d56a1b5dead47f7f
Vasavi2812 <86615056+Vasavi2812@users.noreply.github.com>
2021-07-20 22:07:15 +0530

    Create balanced.java

---
+++ balanced.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class balanced
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the size of an array");
+int i,n=sc.nextInt();
+int a[]=new int[n];
+for(i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+Stack<Integer> s=new Stack<Integer>();
+Stack<Integer> s1=new Stack<Integer>();
+Stack<Integer> s2=new Stack<Integer>();
+for(i=0;i<n;i++)
+{
+s.add(a[i]);
+}
+for(int j=0;j<s.size();j++)
+{
+if(s.get(j)%2!=0)
+s1.add(s.get(j));
+else
+s2.add(s.get(j));
+}
+if(s1.size()==s2.size())
+System.out.println("stack is balanced");
+else
+System.out.println("stack not balanced");
+}
+}
+
+    
